268b66175d4b5103f0e42a2e136b144be813652d
zalman <zal80b@gmail.com>
2019-12-24 22:31:45 -0500

    Endof mac changer program

--- mac_changer.py
+++ mac_changer.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface = "wlan0"
-new_mac = "00:11:22:33:44:77"
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info.")
+    return  options
 
-print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-# subprocess.call("ifconfig wlan0 down", shell=True)
-# subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
-# subprocess.call("ifconfig wlan0 up", shell=True)
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
